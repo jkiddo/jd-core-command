2bab224a26be43b4e7f596fac85cce0b74e3526e
Jens Krististian Villadsen <jenskristianvilladsen@gmail.com>
2012-07-10 11:48:12 +0200

    * Added source for test file
    Signed-off-by: Jens Krististian Villadsen <jenskristianvilladsen@gmail.com>

---
+++ decompiler/testser.java
@@ -0,0 +1,20 @@
+
+public class testser
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+System.out.println("Yada");
+
+int i = 0; 
+
+int j = 10;
+
+System.out.println(i+j);
+	}
+
+}
